8403735add3f8249e8b366071c3f96fbc405d15a
Julien Fraichot <jfraichot@learningmachine.com>
2020-07-28 15:13:42 +0200

    pass certificate from command line

---
+++ callVerify.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+async function readData (path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(JSON.parse(data));
+    });
+  })
+}
+
+async function callVerify () {
+  const certificate = await readData(process.argv[2]);
+  const res = await fetch('http://localhost:3000/verify', {
+    method: 'POST',
+    body: JSON.stringify({
+      certificate
+    }),
+    headers: {'Content-Type': 'application/json'}
+  }).then(res => {
+    const decoded = res.json();
+    return decoded;
+  }).catch (err => {
+    throw new Error(err);
+  });
+  console.log(res);
+}
+
+callVerify();
